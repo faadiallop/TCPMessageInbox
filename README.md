bfb439b15573a6c02e75adf73f287fce89b5f6f8
Faa Diallo <faadiallop@gmail.com>
2023-09-14 23:01:53 -0700

    Reorganization of files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# TCPMessageInbox
->This simple TCP service receives a message and echoes that message back the client.
+# NetworkEchoer 
+>This simple TCP/UDP server receives a message and echoes that message back the client.
---
+++ TCPEchoer/server.py
@@ -0,0 +1,21 @@
+#Created 09/14/2023
+#Creator: Faa Diallo
+#This is a TCP server that receives a message and echos it back to
+#ths client. After that it closes the connection.
+import socket
+
+HOST = ""
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    print(f"Listening on port {PORT}")
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
